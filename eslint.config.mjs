117db4b065c2aba9c8b1b7974928025be4225318
Andres Guerrero <dev.andres.guerrero@gmail.com>
2025-09-19 22:49:43 -0500

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
 
 export default tseslint.config(
   {
@@ -17,6 +18,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
+      parser: tsParser,
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
@@ -26,6 +28,8 @@ export default tseslint.config(
   },
   {
     rules: {
+      'newline-before-return': 2,
+      'react/prop-types': 0,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
