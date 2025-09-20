77910455129927b2a71ee8e987b7e9a97b0e10bb
Andres Guerrero <dev.andres.guerrero@gmail.com>
2025-09-20 01:06:22 -0500

    Settings prettier eslint husky lintstaged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,63 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import pluginImportHelpers from "eslint-plugin-import-helpers";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
 
-export default tseslint.config(
+export default [
   {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
+    ignores: ["dist", "node_modules", "coverage"],
+    files: ["**/*.ts"],
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      parser: tsParser,
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+      parser: tsparser,
+      sourceType: "module",
     },
-  },
-  {
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+      "import-helpers": pluginImportHelpers,
+      "unused-imports": pluginUnusedImports,
+    },
     rules: {
-      'newline-before-return': 2,
-      'react/prop-types': 0,
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "import-helpers/order-imports": [
+        "warn",
+        {
+          newlinesBetween: "ignore",
+          groups: [
+            ["/^@nestjs/", "module"],
+            "/^@//",
+            ["parent", "sibling", "index"],
+          ],
+          alphabetize: { order: "asc", ignoreCase: true },
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          "endOfLine": "auto",
+          "bracketSpacing": true,
+          "printWidth": 150,
+          "semi": true,
+          "singleQuote": false,
+          "tabWidth": 2,
+          "trailingComma": "all",
+        }
+      ],
     },
   },
-);
+];
